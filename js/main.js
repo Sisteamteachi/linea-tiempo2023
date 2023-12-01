424be53f68d98dcf46a9a59cbca536ddd7a37c5b
blacksoftware-100 <jt158161@gmail.com>
2023-12-01 17:36:25 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function(event) {    
+
+    document.getElementById("loading").classList.toggle("loadings")
+    document.getElementById("loading").classList.toggle("esconder")
+    document.getElementById("linea").classList.toggle("esconder")
+});
